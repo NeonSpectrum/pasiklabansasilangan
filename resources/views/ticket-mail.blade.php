26939e6f14c656702357a2f3ef5a12dc7958d00b
neonspectrum <youngskymann@gmail.com>
2019-02-18 14:56:03 +0800

    update

--- resources/views/ticket-mail.blade.php
+++ resources/views/ticket-mail.blade.php
@@ -1,3 +1,3 @@
 Dear {{ $name }},
 <br><br>
-This is to confirm your pre-registration in the "Pasiklaban sa Silangan Event". It is important to keep/bring your barcode during your scheduled time on February 26, 2019 at the UE Theater. NO BARCODE, NO ENTRY policy.
+This is to confirm your pre-registration in the "Pasiklaban sa Silangan Event". It is important to keep/bring your barcode during your scheduled time on February 26, 2019 at the UE Theater. NO QR CODE, NO ENTRY policy.
