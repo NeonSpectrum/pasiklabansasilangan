e190eeb43a8f401d769d81f94af9848fac31416c
neonspectrum <youngskymann@gmail.com>
2019-02-22 08:51:53 +0800

    set execution time and memory limit to fix pdf loading issue

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -10,6 +10,8 @@ class ReportController extends Controller {
    * @return mixed
    */
   protected function show() {
+    ini_set('max_execution_time', 300);
+    ini_set('memory_limit', '512M');
     $pdf = \PDF::loadView('pdf.report', ['data' => User::all()])->setPaper('a4', 'landscape');
 
     return $pdf->stream();
