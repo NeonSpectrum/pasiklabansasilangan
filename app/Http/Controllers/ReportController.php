ec598c4267c67f568d8a18755cd9c5e797f39d6c
neonspectrum <youngskymann@gmail.com>
2019-02-22 20:56:02 +0800

    set max execution time to 0

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -10,7 +10,7 @@ class ReportController extends Controller {
    * @return mixed
    */
   protected function show() {
-    ini_set('max_execution_time', 300);
+    ini_set('max_execution_time', 0);
     ini_set('memory_limit', '512M');
     $pdf = \PDF::loadView('pdf.report', ['data' => User::all()])->setPaper('a4', 'landscape');
 
