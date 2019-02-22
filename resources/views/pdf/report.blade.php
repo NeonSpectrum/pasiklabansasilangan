eee7b1642d1365cb41d40b89e0d7451748313312
neonspectrum <youngskymann@gmail.com>
2019-02-22 09:50:16 +0800

    update

--- resources/views/pdf/report.blade.php
+++ resources/views/pdf/report.blade.php
@@ -5,6 +5,7 @@
   th, td {
     padding: 10px;
     border: 1px solid black;
+    word-wrap: break-word;
   }
   tfoot>tr>th{
     border:none;
@@ -15,32 +16,34 @@
   <small>As of {{ date("F d, Y h:i:s A") }}</small>
 </center>
 <br>
-<table width="100%" cellspacing="0">
-  <thead>
-    <tr>
-      <th width="5%">ID</th>
-      <th width="15%">Name</th>
-      <th width="15%">Email Address/Reference Number</th>
-      <th width="10%">Strand</th>
-      <th width="15%">Preferred School</th>
-      <th width="15%">Preferred Program/Course</th>
-      <th width="15%">Parent's Contact Number</th>
-      <th width="10%">Date Registered</th>
-    </tr>
-  </thead>
-  <tbody>
-    @foreach($data as $id => $row)
+<div>
+  <table width="100%" cellspacing="0" style="table-layout: fixed;">
+    <thead>
       <tr>
-        <td>{{ $id + 1 }}</td>
-        <td>{{ $row->name }}</td>
-        <td>{{ $row->email_address }}<br/>{{ $row->reference_number }}</td>
-        <td>{{ $row->strand }}</td>
-        <td>{{ $row->preferred_school }}</td>
-        <td>{{ $row->preferred_program }}</td>
-        <td>{{ $row->parents_contact_number }}</td>
-        <td>{{ $row->created_at->format('F d, Y') }}</td>
+        <th style="width: 5%">ID</th>
+        <th style="width: 15%">Name</th>
+        <th style="width: 15%">Email Address/Reference Number</th>
+        <th style="width: 10%">Strand</th>
+        <th style="width: 15%">Preferred School</th>
+        <th style="width: 15%">Preferred Program/Course</th>
+        <th style="width: 15%">Parent's Contact Number</th>
+        <th style="width: 10%">Date Registered</th>
       </tr>
-    @endforeach
-  </tbody>
-</table>
+    </thead>
+    <tbody>
+      @foreach($data as $id => $row)
+        <tr>
+          <td style="width: 5%">{{ $id + 1 }}</td>
+          <td style="width: 15%">{{ $row->name }}</td>
+          <td style="width: 15%">{{ $row->email_address }}<br/>{{ $row->reference_number }}</td>
+          <td style="width: 10%">{{ $row->strand }}</td>
+          <td style="width: 15%">{{ $row->preferred_school }}asdasdadasdasdasdasdasdasdas</td>
+          <td style="width: 15%">{{ $row->preferred_program }}</td>
+          <td style="width: 15%">{{ $row->parents_contact_number }}</td>
+          <td style="width: 10%">{{ $row->created_at->format('F d, Y') }}</td>
+        </tr>
+      @endforeach
+    </tbody>
+  </table>
+</div>
 <div style="position:absolute;bottom:0;width:100%;text-align:center">CCSS Research and Development Unit</div>
